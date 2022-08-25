7085bea6f3b72ceef3c9f10bcd9e16218bdb2203
Suraj G <105499708+surajsunny07@users.noreply.github.com>
2022-08-25 11:22:24 +0530

    Update deploy.js

--- wave portal/scripts/deploy.js
+++ wave portal/scripts/deploy.js
@@ -1,25 +1,43 @@
 const main = async () => {
-    const [deployer] = await hre.ethers.getSigners();
-    const accountBalance = await deployer.getBalance();
-  
-    console.log("Deploying contracts with account: ", deployer.address);
-    console.log("Account balance: ", accountBalance.toString());
-  
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-  
-    console.log("WavePortal address: ", waveContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let waveTxn = await waveContract.wave('This is wave #1');
+  await waveTxn.wait();
+
+  let waveTxn = await waveContract.wave('This is wave #2');
+  await waveTxn.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.addresss);
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
